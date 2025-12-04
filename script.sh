ebb080c540bb2436c842a2c9c66fd658dffbfa0b
joreyesag <joreyesag@uide.edu.ec>
2025-12-04 12:57:31 +0000

    Script

---
+++ script.sh
@@ -0,0 +1,10 @@
+#Escribe la frase "hola Linux" dentro del archivo llamado
+echo "hola Linux" > archivo.txt
+#Muestra el contenido completo del archivo archivo.txt en la terminal.
+cat archivo.txt
+#Copia el archivo original archivo.txt y crea un duplicado llamado copia.txt
+cp archivo.txt copia.txt
+#Lista los archivos y directorios del lugar actual.
+ls -l
+#Borra de forma permanente el archivo archivo.txt del sistema
+rm archivo.txt
