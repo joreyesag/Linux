97d1ae309e9d6ba5028eb44c6181e56950dfdf9a
joreyesag <joreyesag@uide.edu.ec>
2025-12-04 13:19:59 +0000

    Script 2 parte 2

---
+++ script2.sh
@@ -0,0 +1,8 @@
+#imprime toda la informacion del sistema
+uname -a
+#Informacion detallada de la arquitectura
+lscpu
+#Cantidad total de memoria 
+free -h
+#Informacion sobre el uso del estado
+df -h
